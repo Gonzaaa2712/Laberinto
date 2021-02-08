c31fdd25a28dcd9d89f7aec088b263a21ee0f14b
Gonzaaa2712 <gonzalo.ramiro27@gmail.com>
2021-02-08 12:03:10 +0100

    Prueba commit desde consola 2

---
+++ ED2PracticaLaberinto/src/Laberinto.java
@@ -0,0 +1,118 @@
+public class Laberinto {
+
+    private final int TAMAÑO;
+    private char[][] tablero;
+    private Coordenada entrada, salida;
+    private Pila pilaCaminos;
+    private boolean[][] visitados;
+
+
+    public Laberinto(int tamaño, char[][] tablero, Coordenada entrada, Coordenada salida) {
+        this.TAMAÑO = tamaño;
+        this.tablero = tablero;
+        this.entrada = entrada;
+        this.salida = salida;
+        this.pilaCaminos = new Pila();
+        this.visitados = new boolean[TAMAÑO][TAMAÑO];
+        for (int i = 0; i < TAMAÑO; i++) {
+            for (int j = 0; j < TAMAÑO; j++) {
+                this.visitados[i][j] = false;
+            }
+        }
+    }
+
+    public void mostrar() {
+        for (int i = 0; i <= TAMAÑO + 1; i++) {
+            System.out.print("X");
+        }
+        System.out.println();
+        for (int i = 0; i < TAMAÑO; i++) {
+            if (i != entrada.getFila()) {
+                System.out.print("X");
+            } else {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < TAMAÑO; j++) {
+                System.out.print(tablero[i][j]);
+            }
+            if (i != salida.getFila()) {
+                System.out.println("X");
+            } else {
+                System.out.println(" ");
+            }
+        }
+        for (int i = 0; i <= TAMAÑO + 1; i++) {
+            System.out.print("X");
+        }
+        System.out.println();
+        System.out.println();
+    }
+
+
+
+    private boolean esCaminoValidoNuevo(Coordenada coordenada) {
+
+        boolean caminoValido = false;
+
+        if(coordenada.getColumna() < TAMAÑO && coordenada.getColumna() >= 0 && coordenada.getFila() >= 0 && coordenada.getFila() < TAMAÑO ){ // Primer requisito para que sea un camino valido
+
+            if(tablero[coordenada.getFila()][coordenada.getColumna()] == ' '){ // La coordenada es camino ya que es un espacio en blanco
+
+                if(!visitados[coordenada.getFila()][coordenada.getColumna()]){ // Si la coordenada no ha sido visitada
+                    caminoValido = true;
+                }
+            }
+
+        }
+
+
+        return caminoValido;
+    }
+
+
+    public boolean existeCamino() {
+        boolean existeCamino = true;
+        boolean salidaEncontrada = false;
+        Coordenada coordenadaActual;
+
+
+
+        Pila pilaCaminos = new Pila();
+        pilaCaminos.apilar(entrada);
+        visitados[entrada.getFila()][entrada.getColumna()] = true;
+        while(existeCamino && !salidaEncontrada){
+            coordenadaActual = pilaCaminos.desapilar();
+            coordenadaActual.mostrar();
+            Coordenada[] posicionesAExplorar = {coordenadaActual.arriba(),coordenadaActual.abajo(),coordenadaActual.izquierda(), coordenadaActual.derecha()};
+            for(int i = 0; i < posicionesAExplorar.length; i++){
+               Coordenada posicionAComprobar = posicionesAExplorar[i];
+               if(esCaminoValidoNuevo(posicionAComprobar)){
+                   pilaCaminos.apilar(posicionAComprobar);
+                   visitados[posicionAComprobar.getFila()][posicionAComprobar.getColumna()] = true;
+               }
+
+            }
+
+            if(pilaCaminos.getNumElementos() == 0){ // Si en la pila no hay elementos apìlados significa que no existe camino posible
+                existeCamino = false;
+            }
+
+         /*   if(visitados[salida.getFila()][salida.getColumna()] == true){ // Como al apilar elementos a la vez los consideramos como visitados, en caso de que la salida esté en la matriz de visitados, existirá
+                salidaEncontrada = true;
+                salida.mostrar();
+            } */
+
+            if(coordenadaActual.iguales(salida)){
+                salidaEncontrada = true;
+            }
+
+
+        }
+
+
+
+        return existeCamino;
+    }
+
+
+}
---
+++ ED2PracticaLaberinto/src/Principal.java
@@ -0,0 +1,42 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        final int TAMAÑO = 6;
+        Coordenada entrada, salida;
+        char[][] tabla = new char[TAMAÑO][TAMAÑO];
+        String fila0 = "X     ";
+        String fila1 = "    X ";
+        String fila2 = "X X XX";
+        String fila3 = "   XX ";
+        String fila4 = " X XX ";
+        String fila5 = " X    ";
+        tabla[0] = fila0.toCharArray();
+        tabla[1] = fila1.toCharArray();
+        tabla[2] = fila2.toCharArray();
+        tabla[3] = fila3.toCharArray();
+        tabla[4] = fila4.toCharArray();
+        tabla[5] = fila5.toCharArray();
+        entrada = new Coordenada(1,0);
+        salida = new Coordenada(3, TAMAÑO - 1);
+        System.out.println("USANDO LA CLASE Pila:");
+        Laberinto laberinto = new Laberinto(TAMAÑO, tabla, entrada, salida);
+        laberinto.mostrar();
+        if (laberinto.existeCamino()) {
+            System.out.println("Existe camino en el Laberinto desde la entrada a la salida");
+	    //Añadiendo commit desde Consola
+        } else {
+            System.out.println("No existe camino desde la entrada a la salida \n");
+        }
+
+        // Probando el uso de los Commits en Github
+        System.out.println("USANDO STACK :\n");
+        LaberintoStack laberintoStack = new LaberintoStack(TAMAÑO, tabla, entrada, salida);
+        laberintoStack.mostrar();
+        if (laberintoStack.existeCamino()) {
+            System.out.println("Existe camino en el Laberinto desde la entrada a la salida");
+        } else {
+            System.out.println("No existe camino desde la entrada a la salida");
+        }
+
+    }
+}
